43c579ce5bbec9abb2738898888e29117928b314
Nandhana Rajendran <nandhanarajendran2004@gmail.com>
2025-02-15 23:57:05 +0530

    Dark mode added

---
+++ script.js
@@ -0,0 +1,15 @@
+//toggle mode
+const mode = document.querySelector('.mode')
+const icon = document.querySelector('.fa-moon')
+
+mode.addEventListener('click',function(){
+    document.body.classList.toggle('dark-mode');
+
+    if(document.body.classList.contains('dark-mode')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
+})
